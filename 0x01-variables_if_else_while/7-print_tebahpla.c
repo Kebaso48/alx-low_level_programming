544cf552d5aa0f94ff93803bb82351554bc35525
Kebaso48 <kebasovictor9@gmail.com>
2022-12-09 07:00:23 -0800

    alpha in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int al;
+
+	for (al = 'z'; al >= 'a'; al--)
+		putchar(al);
+
+	putchar('\n');
+
+	return (0);
+}
