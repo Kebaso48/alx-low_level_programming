92dbde3c0d5064b31c864d3fc7af85173c212408
Kebaso48 <kebasovictor9@gmail.com>
2022-12-22 00:46:05 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes string using rot13
+ * @str: the sting to be encoded
+ * Return: pointer to string to be encoded
+ */
+char *rot13(char *str)
+{
+	int i = 0, j;
+	char alpha[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	while (str[i])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i]  = rot13key[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
