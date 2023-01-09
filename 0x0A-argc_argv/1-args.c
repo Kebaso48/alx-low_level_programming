44065cd4d5d8ab3e8c99fcf23c9815ea0286ab61
Kebaso48 <kebasovictor9@gmail.com>
2023-01-09 05:04:58 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed onto it
+ * @argc: the number of programs supplied
+ * @argv: the array of ointers to argument
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
+	
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed onto it
+ * @argc: the number of programs supplied
+ * @argv: the array of ointers to argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	
+	return (0);
+}
+	
