dfe71182c23fae6b12defdc35190ab565f9fcd9c
Kebaso48 <kebasovictor9@gmail.com>
2023-01-19 10:47:20 -0800

    function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,3 +12,34 @@ int main(int argc, char *argv[])
 	int byte, i;
 	int (*address)(int, char **) = main;
 	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	byte = atoi(argv[1]);
+
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < byte; i++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (i == byte - 1)
+			continue;
+
+		printf(" ");
+
+		address++;
+	}
+	printf("\n");
+
+	return (0);
+}
