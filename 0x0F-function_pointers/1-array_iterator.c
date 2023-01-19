7c106dc1a0b82ba35d699d74687cc95e52d2897e
Kebaso48 <kebasovictor9@gmail.com>
2023-01-19 10:31:17 -0800

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameteron esch array element
+ * array_iterator - executes a function given as a parameter
+ * on esch array element
  * @array: the array
  * @size: the size of array
  * @action: pointer to action
@@ -13,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	while (size-- > 0)
 	{
-		action (*array);
+		action(*array);
 		array++;
 	}
 }
