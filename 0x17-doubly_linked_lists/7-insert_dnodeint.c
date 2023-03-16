30acdc839cfd72df34704131bfc20e2342f32ad6
Kebaso48 <kebasovictor9@gmail.com>
2023-03-16 05:56:50 -0700

    crack

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts new node at given position
+ * @h: pointer to head of dlistint_t list
+ * @idx: position to insert new node
+ * @n: integer contained in the new node
+ * Return: NULL if function fails, otherwise address of new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next->prev = new;
+	temp->next = new;
+
+	return (new);
+}
